f18d790dd32dbc48ecdff0e3e9ec850d475582e8
bonitobonito2 <zaal.tsagareli.1btu.edu.ge>
2023-02-06 16:35:45 +0400

    added some complex calculation logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,15 +5,48 @@ function parsingUserInputValue(){
     return parseInt(userInput.value)
 }
 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber ){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription);
+
+}
 
 function add() {
     const enteredNumber = parsingUserInputValue()
-    const calcDescription = `${currentResult} + ${enteredNumber}`
+    const initialResult = currentResult
     currentResult = currentResult + enteredNumber
-    outputResult(currentResult, calcDescription);
-
+createAndWriteOutput("+", initialResult, enteredNumber)
 } 
+
+function subtract(){
+    const enteredNumber = parsingUserInputValue()
+    const initialResult = currentResult
+    currentResult = currentResult - enteredNumber
+    createAndWriteOutput("-", initialResult, enteredNumber)
+
+}
+
+function multiply(){
+    const enteredNumber = parsingUserInputValue()
+    const initialResult = currentResult
+    currentResult = currentResult * enteredNumber
+    createAndWriteOutput("*", initialResult, enteredNumber)
+}
+
+function devide(){
+    const enteredNumber = parsingUserInputValue()
+    const initialResult = currentResult
+    currentResult = currentResult / enteredNumber
+    createAndWriteOutput("/", initialResult, enteredNumber)
+}
+
+
 addBtn.addEventListener("click", add )
+divideBtn.addEventListener('click',devide)
+multiplyBtn.addEventListener('click',multiply)
+
+subtractBtn.addEventListener('click',subtract)
+
 
 
 
