7196040fd72ce192c90ee497026ca7abe1ea4cab
beroo20 <mariamiiiberuashviligmail.com>
2023-02-28 23:48:22 +0400

    new

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,26 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries =
 
 function parsingUserInputValue(){
     return parseInt(userInput.value)
+}
+function writeToLog(
+    operationIdentifier,
+    prevResult,
+    operationNUmber,
+    newResult
+) {
+    const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNUmber,
+    result: newResult
+}
+
+logEntries.push(logEntry)
+console.log(logEntries)
+
 }
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber ){
@@ -14,30 +32,34 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber ){
 function add() {
     const enteredNumber = parsingUserInputValue()
     const initialResult = currentResult
-    currentResult = currentResult + enteredNumber
-createAndWriteOutput("+", initialResult, enteredNumber)
+    currentResult += enteredNumber
+    createAndWriteOutput("+", initialResult, enteredNumber)
+    writeToLog("ADD", initialResult, enteredNumber,  )
 } 
+
 
 function subtract(){
     const enteredNumber = parsingUserInputValue()
     const initialResult = currentResult
-    currentResult = currentResult - enteredNumber
+    currentResult -=  enteredNumber
     createAndWriteOutput("-", initialResult, enteredNumber)
-
+    writeToLog("subtract", initialResult, enteredNumber,  )
 }
 
 function multiply(){
     const enteredNumber = parsingUserInputValue()
     const initialResult = currentResult
-    currentResult = currentResult * enteredNumber
+    currentResult *= enteredNumber
     createAndWriteOutput("*", initialResult, enteredNumber)
+    writeToLog("multiply", initialResult, enteredNumber,  )
 }
 
 function devide(){
     const enteredNumber = parsingUserInputValue()
     const initialResult = currentResult
-    currentResult = currentResult / enteredNumber
+    currentResult /= enteredNumber
     createAndWriteOutput("/", initialResult, enteredNumber)
+    writeToLog("devide", initialResult, enteredNumber,  )
 }
 
 
